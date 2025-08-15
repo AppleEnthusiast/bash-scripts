c4bb4968d6a8884aceca5818d79fdca5b9ea7642
Teoman Dogueri <Teoman.dogueri@icloud.com>
2025-08-16 00:31:21 +0200

    Remove read commands to take number inputs from user from each if statement

--- basic_math.sh
+++ basic_math.sh
@@ -21,32 +21,24 @@ then
 	echo "Addition"
 	# read -p displays text and prompt in one line
 	# read - r reads escape sequences as text
-	read -r -p "Enter first number: " num1
-	read -r -p "Enter second number: " num2
 fi
 
 if [ "$choice" = "b" ]
 then
 	echo "Subtraction"
 
-	read -r -p "Enter first number: " num1
-	read -r -p "Enter second number: " num2
 fi
 
 if [ "$choice" = "c" ]
 then
 	echo "Multiplikation"
 
-	read -r -p "Enter first number: " num1
-	read -r -p "Enter second number: " num2
 fi
 
 if [ "$choice" = "d" ]
 then
 	echo "Division"
 
-	read -r -p "Enter first number: " num1
-	read -r -p "Enter second number: " num2
 fi
 
 echo
